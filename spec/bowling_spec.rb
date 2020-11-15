73dfb47b7cbd378b88a59da193ff98db658621db
Tomohiro Nishimura <tomohiro68@gmail.com>
2020-11-16 00:58:50 +0900

    use context instead of describe

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe Bowling do
     let(:game) { Bowling.new }
     subject { game.score }
 
-    describe '全てガーターの場合' do
+    context '全てガーターの場合' do
       before { 20.times { game.roll(0) } }
       it { is_expected.to eq 0 }
     end
 
-    describe '全て１ピン倒した場合' do
+    context '全て１ピン倒した場合' do
       before { 20.times { game.roll(1) } }
       it { is_expected.to eq 20 }
     end
 
-    describe '全てストライクだった場合' do
+    context '全てストライクだった場合' do
       before { 11.times { game.roll(10) } }
       it { is_expected.to eq 300 }
     end
 
-    describe '全て５ピンずつ倒した場合 (スペア)' do
+    context '全て５ピンずつ倒した場合 (スペア)' do
       before { 21.times { game.roll(5) } }
       it { is_expected.to eq 155 }
     end
 
-    describe 'ストライクの次にスペアだった場合' do
+    context 'ストライクの次にスペアだった場合' do
       before do
         game.roll(10)
         game.roll(2)
